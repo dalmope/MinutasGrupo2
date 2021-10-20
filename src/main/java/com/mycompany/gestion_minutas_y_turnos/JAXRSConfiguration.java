0f8dcb7d34e5cc415267514427709e523f1d09c1
dalmope <eukadaju@gmail.com>
2021-10-20 18:38:43 -0500

    first commit, login not funtional

---
+++ src/main/java/com/mycompany/gestion_minutas_y_turnos/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.gestion_minutas_y_turnos;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
